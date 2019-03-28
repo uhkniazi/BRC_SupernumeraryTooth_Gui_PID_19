df9e7ceca49f63844febf765e11b05d6e4a80d1f
Umar Niazi <uhkniazi@gmail.com>
2019-03-28 14:00:54 +0000

    figures with summary tables of contrasts

--- interactionContrasts/11_vennDiagram.R
+++ interactionContrasts/11_vennDiagram.R
@@ -4,7 +4,8 @@
 # Date: 14/03/2019
 
 source('header.R')
-
+setwd(gcswd)
+setwd('interactionContrasts/')
 lFiles = list.files('results/', pattern='DEAnalysis*', full.names = T, ignore.case = T)
 
 ldfData = lapply(lFiles, function(x) as.data.frame(read.csv(x, header=T, row.names=1, stringsAsFactors = F)))
@@ -35,29 +36,20 @@ names(ldfData)
 dfContrast1.sub = ldfData[[1]][ldfData[[1]]$adj.P.Val < 0.1,]
 dfContrast2.sub = ldfData[[2]][ldfData[[2]]$adj.P.Val < 0.1,]
 dfContrast3.sub = ldfData[[3]][ldfData[[3]]$adj.P.Val < 0.1,]
-dfContrast4.sub = ldfData[[4]][ldfData[[4]]$adj.P.Val < 0.1,]
-dfContrast5.sub = ldfData[[5]][ldfData[[5]]$adj.P.Val < 0.1,]
-dfContrast6.sub = ldfData[[6]][ldfData[[6]]$adj.P.Val < 0.1,]
-dfContrast7.sub = ldfData[[7]][ldfData[[7]]$adj.P.Val < 0.1,]
-dfContrast8.sub = ldfData[[8]][ldfData[[8]]$adj.P.Val < 0.1,]
-dfContrast9.sub = ldfData[[9]][ldfData[[9]]$adj.P.Val < 0.1,]
 
 
 library(VennDiagram)
 
 # create a list for overlaps
-lVenn = list(rownames(dfContrast1.sub), rownames(dfContrast2.sub), rownames(dfContrast3.sub),
-             rownames(dfContrast4.sub), rownames(dfContrast5.sub), rownames(dfContrast6.sub),
-             rownames(dfContrast7.sub), rownames(dfContrast8.sub), rownames(dfContrast9.sub)
+lVenn = list(rownames(dfContrast1.sub), 
+             rownames(dfContrast2.sub), 
+             rownames(dfContrast3.sub)
              )
 names(ldfData)
 names(lVenn) = cvTitle
 # calculate overlaps
 #lVenn.overlap = calculate.overlap(lVenn)
-venn.diagram(lVenn[c(1, 3, 6)], filename = 'results/venn_all_contrasts_MutVsWT.tif', margin=0.1)
-venn.diagram(lVenn[c(2, 4, 5)], filename = 'results/venn_all_contrasts_MutTime.tif', margin=0.1)
-venn.diagram(lVenn[c(7, 8, 9)], filename = 'results/venn_all_contrasts_WTTime.tif', margin=0.1)
-
+venn.diagram(lVenn, filename = 'results/venn_all_contrasts.tif', margin=0.1)
 
 ### repeat the analysis but separate up and down regulated genes
 ## select significant genes
@@ -70,41 +62,17 @@ dfContrast2.down = dfContrast2.sub[dfContrast2.sub$logFC < 0, ]
 dfContrast3.up = dfContrast3.sub[dfContrast3.sub$logFC > 0, ]
 dfContrast3.down = dfContrast3.sub[dfContrast3.sub$logFC < 0, ]
 
-dfContrast4.up = dfContrast4.sub[dfContrast4.sub$logFC > 0, ]
-dfContrast4.down = dfContrast4.sub[dfContrast4.sub$logFC < 0, ]
-
-dfContrast5.up = dfContrast5.sub[dfContrast5.sub$logFC > 0, ]
-dfContrast5.down = dfContrast5.sub[dfContrast5.sub$logFC < 0, ]
-
-dfContrast6.up = dfContrast6.sub[dfContrast6.sub$logFC > 0, ]
-dfContrast6.down = dfContrast6.sub[dfContrast6.sub$logFC < 0, ]
-
-dfContrast7.up = dfContrast7.sub[dfContrast7.sub$logFC > 0, ]
-dfContrast7.down = dfContrast7.sub[dfContrast7.sub$logFC < 0, ]
-
-dfContrast8.up = dfContrast8.sub[dfContrast8.sub$logFC > 0, ]
-dfContrast8.down = dfContrast8.sub[dfContrast8.sub$logFC < 0, ]
-
-dfContrast9.up = dfContrast9.sub[dfContrast9.sub$logFC > 0, ]
-dfContrast9.down = dfContrast9.sub[dfContrast9.sub$logFC < 0, ]
-
 # create a list for overlaps
 lVenn = list(rownames(dfContrast1.up), rownames(dfContrast1.down),
              rownames(dfContrast2.up), rownames(dfContrast2.down), 
-             rownames(dfContrast3.up), rownames(dfContrast3.down),
-             rownames(dfContrast4.up), rownames(dfContrast4.down),
-             rownames(dfContrast5.up), rownames(dfContrast5.down),
-             rownames(dfContrast6.up), rownames(dfContrast6.down),
-             rownames(dfContrast7.up), rownames(dfContrast7.down),
-             rownames(dfContrast8.up), rownames(dfContrast8.down),
-             rownames(dfContrast9.up), rownames(dfContrast9.down)
+             rownames(dfContrast3.up), rownames(dfContrast3.down)
              )
 
 
 #cvTitle = gsub('results//DEAnalysis(\\w+)VsControl.xls', '\\1', names(ldfData))
 cvTitle.up = paste(cvTitle, 'up', sep='-')
 cvTitle.down = paste(cvTitle, 'down', sep='-')
-o = c(1, 10, 2, 11, 3, 12, 4, 13, 5, 14, 6, 15, 7, 16, 8, 17, 9, 18)
+o = c(1, 4, 2, 5, 3, 6)
 cvTitle = c(cvTitle.up, cvTitle.down)[o]
 names(lVenn) = cvTitle
 
@@ -146,3 +114,4 @@ rownames(dfCommonGenes) = (gsub(pattern = '^X', replacement = '', x = rownames(d
 head(dfCommonGenes)
 
 write.csv(dfCommonGenes, file='results/commonDEGenes.xls')
+setwd(gcswd)
